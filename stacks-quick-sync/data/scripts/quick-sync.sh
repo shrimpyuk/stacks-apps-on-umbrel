16fc9bfd01a00f47a9a4777f395356bbe860977d
Jasper <jasperbower@protonmail.com>
2023-05-30 17:47:09 -0700

    Fix quick sync

--- stacks-quick-sync/data/scripts/quick-sync.sh
+++ stacks-quick-sync/data/scripts/quick-sync.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env sh
 
+# Setting default values for variables or use values from environment variables if they exist
 ARCHIVE=${ARCHIVE:-https://archive.hiro.so}
-
 NETWORK=${NETWORK:-mainnet}
-
-SERVICE=${SERVICE:-stacks-blockchain-api}
-
+SERVICE=${SERVICE:-stacks-blockchain}
 RELEASE=${RELEASE:-latest}
-
-POSTGRES_VERSION=${POSTGRES_VERSION:-15}
-
 USER_ID=${USER_ID:-$(id -u):$(id -g)}
+DL_RATE=${DL_RATE:-0}
+POSTGRES_VERSION=${POSTGRES_VERSION:-15}
+POSTGRES_USER=${POSTGRES_USER:-postgres}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}
+POSTGRES_HOST=${POSTGRES_HOST:-postgres}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_DB=${POSTGRES_DB:-postgres}
+IMPORT=${IMPORT:-false}
+EXTRACT=${EXTRACT:-true}
 
+# Set different data file names and URLs based on the service being used
 if [ "$SERVICE" = "stacks-blockchain" ]; then
     DATA_FILE=${DATA_FILE:-${NETWORK}-${SERVICE}-${RELEASE}.tar.gz}
     FILE_URL=${FILE_URL:-${ARCHIVE}/${NETWORK}/${SERVICE}/${DATA_FILE}}
     SHA_URL=${SHA_URL:-${ARCHIVE}/${NETWORK}/${SERVICE}/${NETWORK}-${SERVICE}-${RELEASE}.sha256}
-    IMPORT_DIR=${IMPORT_DIR:-${PWD}/${SERVICE}/}
+    IMPORT_DIR=${IMPORT_DIR:-${PWD}/${SERVICE}}
 elif [ "$SERVICE" = "stacks-blockchain-api" ]; then
     DATA_FILE=${DATA_FILE:-${NETWORK}-${SERVICE}-${RELEASE}.gz}
     FILE_URL=${FILE_URL:-${ARCHIVE}/${NETWORK}/${SERVICE}/${DATA_FILE}}
@@ -33,36 +38,98 @@ TARFILE="${IMPORT_DIR}/${DATA_FILE}"
 
 set -- "${DATA_FILE}"
 
-echo ""
-echo "*********************************"
-echo "Downloading ${SERVICE} Data"
-echo "*********************************"
-echo ""
+# Function to check if necessary commands are available and install if they are not
+check_commands() {
+    # Iterating over each required command and checking if it is installed
+    for cmd in wget tar gzip sha256sum pv; do
+        if ! command -v "$cmd" > /dev/null 2>&1; then
+            echo "${cmd} is not installed. Attempting to install..."
+            apk add --upgrade "$cmd"
+        fi
+    done
+    # Check for pg_restore and nodejs based on the service being used
+    if [ "$SERVICE" = "postgres" ]; then
+        if [ "$IMPORT" = "true" ]; then
+            if ! command -v pg_restore > /dev/null 2>&1; then
+                echo "pg_restore is not installed. Attempting to install postgresql..."
+                apk add --upgrade postgresql"${POSTGRES_VERSION}"
+            fi
+        fi
+    fi
 
-if [ ! -d "${IMPORT_DIR}" ]; then
-    echo ""
-    echo "Creating ${SERVICE} data directory: ${IMPORT_DIR}"
-    echo ""
-    mkdir -p "${IMPORT_DIR}"
-fi
+    if [ "$SERVICE" = "stacks-blockchain-api" ]; then
+        if [ "$IMPORT" = "true" ]; then
+            if ! command -v node > /dev/null 2>&1; then
+                echo "nodejs is not installed. Attempting to install nodejs..."
+                apk add --upgrade nodejs-current
+            fi
+        fi
+    fi
+}
 
-if [ ! -f "${TARFILE}.sha256" ]; then
-    echo ""
-    echo "Retrieving ${SERVICE} data as ${TARFILE}"
-    echo "From ${FILE_URL}"
-    echo ""
-    wget "${SHA_URL}" -O "${TARFILE}.sha256" || {
-        echo "Failed to download ${TARFILE}.sha256"
-        exit 1
-    }
-fi
-if [ ! -f "${TARFILE}" ]; then
-    wget "${FILE_URL}" -O "${TARFILE}" || {
-        echo "Failed to download ${TARFILE}"
-        exit 1
-    }
-fi
+# Function to download files if they do not exist in the specified path
+download_file() {
+    file_url="$1"
+    output_file="$2"
+    dl_rate="$3"
 
+    if [ ! -f "${output_file}" ]; then
+        if [ "${dl_rate}" -eq 0 ]; then
+            echo "Downloading ${output_file}"
+            wget "${file_url}" -O "${output_file}" || {
+                echo "Failed to download ${output_file}"
+                exit 1
+            }
+        else
+            echo "Downloading ${output_file} with rate limit of ${dl_rate}m"
+            wget --limit-rate="${dl_rate}m" "${file_url}" -O "${output_file}" || {
+                echo "Failed to download ${output_file}"
+                exit 1
+            }
+        fi
+    fi
+}
+
+# Function to import data for stacks-blockchain-api and postgres services
+import_files() {
+    file="${1}"
+    file_path="${IMPORT_DIR}/${file}"
+    echo ""
+    echo "**********************************************************"
+    echo "   Importing ${SERVICE} Data. This will take awhile..."
+    echo "**********************************************************"
+    if [ "$SERVICE" = "stacks-blockchain-api" ]; then
+        # Event replay import will only work when run from stacks-blockchain-api directory/docker container
+        if [ -f ./lib/index.js ]; then
+            # For stacks-blockchain-api, kill existing node process and start new one for importing events
+            pkill node
+            node ./lib/index.js import-events --file "${IMPORT_DIR}/stacks-node-events.tsv" --wipe-db --force >> "${IMPORT_DIR}/output.log" 2>&1 &
+            # Waiting for successful import or failed import message
+            while true; do
+            sleep 10
+                if grep -q "Event import and playback successful" "${IMPORT_DIR}/output.log"; then
+                    echo "Event import and playback successful"
+                    break
+                elif grep -q "import-events process failed" "${IMPORT_DIR}/output.log"; then
+                    echo "import-events process failed, trying again..."
+                    # Kill the previous node process
+                    pkill node
+                    # Try running the node command again
+                    node ./lib/index.js import-events --file "${IMPORT_DIR}/stacks-node-events.tsv" --wipe-db --force >> "${IMPORT_DIR}/output.log" 2>&1
+                fi
+            done
+        else
+            return 1
+        fi
+    elif [ "$SERVICE" = "postgres" ]; then
+        # For postgres, use pg_restore to import data
+        PGPASSWORD=$POSTGRES_PASSWORD pg_restore -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -Fc -v "${TARFILE}"
+    fi
+    echo ""
+    echo "Done importing ${SERVICE} Data"
+}
+
+# Function to verify the sha256 checksum of the downloaded file
 check_sha256() {
     file="${1}"
     file_256="${file}.sha256"
@@ -77,31 +144,67 @@ check_sha256() {
     [ "${sha256}" = "${sha256sum}" ] && [ "${filename}" = "${file}" ]
 }
 
-# extract_files() {
-#     file="${1}"
-#     file_path="${IMPORT_DIR}/${file}"
+# Function to extract the downloaded files
+extract_files() {
+    file="${1}"
+    file_path="${IMPORT_DIR}/${file}"
+    echo ""
+    echo "**********************************************************"
+    echo "   Extracting ${SERVICE} Data. This will take awhile..."
+    echo "**********************************************************"
+    if [ "$SERVICE" = "stacks-blockchain" ]; then
+        # If service is stacks-blockchain, use tar to extract the file
+        pv -ptebar "${TARFILE}" | tar -xzf - -C "${IMPORT_DIR}" >/dev/null
+    elif [ "$SERVICE" = "stacks-blockchain-api" ]; then
+        # If service is stacks-blockchain-api, use gzip to decompress the file
+        gzip -dc "${TARFILE}" > "${IMPORT_DIR}/stacks-node-events.tsv"
+    fi
+    echo ""
+    echo "Done extracting ${SERVICE} Data"
+}
 
-#     if [ "$SERVICE" = "stacks-blockchain" ]; then
-#         tar -xzf "${TARFILE}" -C "${IMPORT_DIR}"/"${file}"
-#     elif [ "$SERVICE" = "stacks-blockchain-api" ]; then
-#         gzip -dc "${TARFILE}" > "${IMPORT_DIR}/stacks-node-events.tsv"
-#     fi
-# }
+# Start of the main execution
+echo ""
+echo "**********************************************************"
+echo "        Running setup script for ${SERVICE} Data"
+echo "**********************************************************"
+echo ""
 
+# Start of the main execution
+check_commands
+
+# Check if import directory exists, if not create it
+if [ ! -d "${IMPORT_DIR}" ]; then
+    echo ""
+    echo "Creating ${SERVICE} data directory: ${IMPORT_DIR}"
+    echo ""
+    mkdir -p "${IMPORT_DIR}"
+fi
+
+echo ""
+echo "Retrieving ${SERVICE} data as ${TARFILE}"
+echo "From ${FILE_URL}"
+echo ""
+# Download sha256 and actual service file
+download_file "${SHA_URL}" "${TARFILE}.sha256" "${DL_RATE}"
+download_file "${FILE_URL}" "${TARFILE}" "${DL_RATE}"
+
+# Iterate over all the files to check sha256, extract and import them
 for FILE in "$@"; do
     echo ""
     echo "Checking sha256 of: ${TARFILE}"
     echo ""
 
+    # Check sha256 checksum of downloaded file
     if ! check_sha256 "${FILE}"; then
         echo "[ Warning ] - sha256 mismatch"
         echo "    - Removing ${FILE}, re-attempting sha256 verification"
         rm -f "${IMPORT_DIR}/${FILE}" "${IMPORT_DIR}/${FILE}.sha256"
-        wget "${SHA_URL}" -O "${TARFILE}.sha256" || {
+        download_file "${SHA_URL}" "${TARFILE}.sha256" || {
             echo "Failed to re-download ${TARFILE}.sha256"
             exit 1
         }
-        wget "${FILE_URL}" -O "${TARFILE}" || {
+        download_file "${FILE_URL}" "${TARFILE}" "${DL_RATE}" || {
             echo "Failed to re-download ${TARFILE}"
             exit 1
         }
@@ -111,33 +214,55 @@ for FILE in "$@"; do
             rm -f "${IMPORT_DIR}/${FILE}" "${IMPORT_DIR}/${FILE}.sha256"
             exit 1
         fi
+    else
+        sha256sum=$(sha256sum "${IMPORT_DIR}/${FILE}" | awk {'print $1'})
+        echo ""
+        printf "  - %-25s: %-20s Matched sha256 with %s\n" "${FILE}" "${sha256sum}" "${FILE}.sha256"
+        echo ""
+
+        # If file exists, extract and import it based on the service
+        if [ -f "${IMPORT_DIR}/${FILE}" ]; then
+            if [ "$SERVICE" = "stacks-blockchain" ] || [ "$SERVICE" = "stacks-blockchain-api" ]; then
+                if [ "$EXTRACT" = "true" ]; then
+                    echo "Extracting ${SERVICE} files: ${FILE}"
+                    extract_files "${FILE}"
+                    if [ "$SERVICE" = "stacks-blockchain-api" ]; then
+                        if [ "$IMPORT" = "true" ]; then
+                            echo "Importing ${SERVICE} files: ${FILE}"
+                            import_files "${FILE}"
+                        fi
+                    fi
+                fi
+            else #postgres
+                if [ "$IMPORT" = "true" ]; then
+                    echo "Importing ${SERVICE} files: ${FILE}"
+                    import_files "${FILE}"
+                fi
+            fi
+        fi
     fi
-
-    # if [ ! -f "${IMPORT_DIR}/${FILE}" ] || [ ! -f "${IMPORT_DIR}/${FILE}.sha256" ]; then
-    #     echo "Extracting ${SERVICE} files: ${FILE}"
-    #     extract_files "${FILE}"
-    # fi
-
-    sha256sum=$(sha256sum "${IMPORT_DIR}/${FILE}" | awk {'print $1'})
-    echo ""
-    printf "  - %-25s: %-20s Matched sha256 with %s\n" "${FILE}" "${sha256sum}" "${FILE}.sha256"
-    echo ""
 done
 
+# Setting the ownership of the import directory to the user
 echo "Setting dir ownership"
 echo "cmd: chown -R ${USER_ID} ${IMPORT_DIR}"
 chown -R "${USER_ID}" "${IMPORT_DIR}"
+
+# Cleanup: Removing downloaded files
 echo ""
-# if [ "${SERVICE}" = "stacks-blockchain" ] || [ "${SERVICE}" = "stacks-blockchain-api" ]; then
-#     echo "Removing download archive: ${TARFILE} & ${TARFILE}.sha256"
-#     rm -f "${TARFILE}"
-#     rm -f "${TARFILE}.sha256"
-# else
-#     rm -f "${TARFILE}.sha256"
-# fi
+echo "Cleaning up downloaded files"
+if [ -f "${IMPORT_DIR}/${FILE}" ]; then
+    echo "Removing download archive: ${TARFILE}"
+    rm -f "${TARFILE}"
+elif [ -f "${IMPORT_DIR}/${FILE}.sha256" ]; then
+    echo "Removing downloaded ${TARFILE}.sha256"
+    rm -f "${TARFILE}.sha256"
+fi
+
+# Script execution complete
 echo ""
 echo "${SERVICE} data downloaded and verified"
-echo
+echo ""
 echo "${SERVICE} data check complete"
 echo ""
 exit 0
--- stacks-quick-sync/data/scripts/run.sh
+++ stacks-quick-sync/data/scripts/run.sh
@@ -4,96 +4,26 @@ sleep=10
 
 echo ""
 echo ""
-echo "*********************************"
+echo "**********************************"
 echo "   Beginning Stacks Quick Sync"
-echo "*********************************"
+echo "**********************************"
 echo ""
 echo ""
-
-chown -R "1000:1000" bns-data event-replay stacks-blockchain gotty scripts
 
 cd /app || exit
 
+chown -R "1000:1000" bns-data event-replay stacks-blockchain gotty scripts
+
 chmod +x scripts/quick-sync.sh scripts/bns.sh
 
-# Download Stacks 1.0 BNS Data not sure this is even necessary when importing event-replay from Hiro
+# Download Stacks 1.0 BNS Data not sure this is even necessary when importing archive from Hiro
 BNS_IMPORT_DIR="/app/bns-data" USER_ID="1000:1000" ./scripts/bns.sh
 
-# Download stacks blockchain api data
-SERVICE=stacks-blockchain-api USER_ID="1000:1000" ./scripts/quick-sync.sh
-
-echo "Extracting API event-replay...."
-gzip -dc event-replay/mainnet-stacks-blockchain-api-latest.gz >event-replay/stacks-node-events.tsv
-echo ""
-echo "Done extracting API event-replay"
-echo ""
-
-if [ -f "/app/event-replay/stacks-node-events.tsv" ]; then
-  # get the size of the local file
-  local_size=$(stat -c%s "/app/event-replay/stacks-node-events.tsv")
-
-  # check if the size is greater than 13GB
-  if [ "$local_size" -lt 13771387596 ]; then
-    echo "Extracted file size is less than 13GB. Extracting again"
-    gzip -dc event-replay/mainnet-stacks-blockchain-api-latest.gz >event-replay/stacks-node-events.tsv
-  else
-    echo "Extracted file size is greater than or equal to 13GB."
-  fi
-else
-  echo "Extracted file not found."
-  gzip -dc event-replay/mainnet-stacks-blockchain-api-latest.gz >event-replay/stacks-node-events.tsv
-fi
+# Download stacks blockchain api data via postgres dump
+SERVICE=postgres USER_ID="1000:1000" IMPORT=true ./scripts/quick-sync.sh
 
 # Download stacks blockchain data
 SERVICE=stacks-blockchain USER_ID="1000:1000" ./scripts/quick-sync.sh
-
-# check if the downloaded file exists and if its size is greater than 40GB as no Content-Length header given from archive
-if [ -f "/app/stacks-blockchain/mainnet-stacks-blockchain-latest.tar.gz" ]; then
-  # get the size of the local file
-  local_size=$(stat -c%s "/app/stacks-blockchain/mainnet-stacks-blockchain-latest.tar.gz")
-
-  # check if the size is greater than 40GB
-  if [ "$local_size" -lt 42949672960 ]; then
-    echo "Downloaded file size is less than 40GB. Re-running quick-sync.sh"
-    SERVICE=stacks-blockchain USER_ID="1000:1000" ./scripts/quick-sync.sh
-  else
-    echo "Downloaded file size is greater than or equal to 40GB."
-  fi
-else
-  echo "Downloaded file not found."
-fi
-
-echo "Extracting Stacks Blockchain Data 93GB+ ...."
-tar -xzf "stacks-blockchain/mainnet-stacks-blockchain-latest.tar.gz" -C "stacks-blockchain" --checkpoint=.1000 --checkpoint-action='ttyout=Extracting %T (%d/sec)\r' &&
-    rm stacks-blockchain/mainnet-stacks-blockchain-latest.tar.gz
-echo ""
-echo "Done extracting Stacks Blockchain Data"
-
-echo ""
-echo "Setting Stacks Blockchain dir ownership"
-echo "chown -R 1000:1000 stacks-blockchain"
-chown -R "1000:1000" stacks-blockchain
-echo "Done"
-
-echo ""
-echo "Running event-replay"
-pkill node
-node ./lib/index.js import-events --file /app/event-replay/stacks-node-events.tsv --wipe-db --force >> /app/gotty/output.log 2>&1 &
-
-# Wait for the "Event import and playback successful" or "import-events process failed" message
-while true; do
-  sleep 10
-  if grep -q "Event import and playback successful" /app/gotty/output.log; then
-    echo "Event import and playback successful"
-    break
-  elif grep -q "import-events process failed" /app/gotty/output.log; then
-    echo "import-events process failed, trying again..."
-    # Kill the previous node process
-    pkill node
-    # Try running the node command again
-    node ./lib/index.js import-events --file /app/event-replay/stacks-node-events.tsv --wipe-db --force
-  fi
-done
 
 while true; do
   echo ""
